4cbec4e0219e5ba2bb000cb76bdaae0a76e52dd0
Stanisław Romek <info@happy.ovh>
2018-08-25 22:27:52 +0200

    Dodanie projektu do repozytorium

---
+++ assets/js/news.js
@@ -0,0 +1,21 @@
+//$(document).ready(function () {
+//
+////  pokazywanie i ukrywanie wstępu posta oraz ikony    
+//    $(".news-box-content").hover(function () {
+//        $(this).find('.news-description').show(300);
+//        $(this).find('.news-icons .icons').show(300);        
+//    }, function () {
+//        $(this).find('.news-description').hide(300);
+//        $(this).find('.news-icons .icons').hide(300);
+//    });
+//
+//
+//    $(".news-box-content1").hover(function () {
+//        $(this).find('.news-description').show(300);
+//        $(this).find('.news-icons1 .icons').show(300);
+//    }, function () {
+//        $(this).find('.news-description').hide(300);
+//        $(this).find('.news-icons1 .icons').hide(300);
+//    });
+//
+//})
